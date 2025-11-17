01d4ae4e4285932d3b22f0c3c04aa8a4dcfaf76e
Arunkv52 <arunkbva5@gmail.com>
2025-11-17 15:36:28 +0530

    new one

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
       <div className="container">
         <div className="flex flex-col gap-1.5 items-center sm:items-start">
           <div className="relative flex items-center w-full">
-            <div className="flex-grow h-px bg-black" />
+            <div className="grow h-px bg-black" />
             <div className="mx-4">
               <Logo />
             </div>
-            <div className="flex-grow h-px bg-black" />
+            <div className="grow h-px bg-black" />
           </div>
           <p className="text-secondary">
             Designed by{" "}
